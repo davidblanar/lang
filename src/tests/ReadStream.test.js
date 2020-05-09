b99acce38a4f204f7b55e799db653d64a3918357
David Blanar <david.blanar@gmail.com>
2020-05-09 11:00:26 +0200

    prettier

--- src/tests/ReadStream.test.js
+++ src/tests/ReadStream.test.js
@@ -1,36 +1,36 @@
 const ReadStream = require("../ReadStream");
 
 describe("ReadStream", () => {
-  const input = "abc";
-  let readStream;
-  beforeEach(() => {
-    readStream = new ReadStream(input);
-  });
+	const input = "abc";
+	let readStream;
+	beforeEach(() => {
+		readStream = new ReadStream(input);
+	});
 
-  it("should peek on next character without discarding it", () => {
-    const peek = readStream.peek();
-    const next = readStream.next();
-    expect(peek).toBe("a");
-    expect(next).toBe("a");
-  });
+	it("should peek on next character without discarding it", () => {
+		const peek = readStream.peek();
+		const next = readStream.next();
+		expect(peek).toBe("a");
+		expect(next).toBe("a");
+	});
 
-  it("should get next character and discard it", () => {
-    const next = readStream.next();
-    const peek = readStream.peek();
-    expect(next).toBe("a");
-    expect(peek).toBe("b");
-  });
+	it("should get next character and discard it", () => {
+		const next = readStream.next();
+		const peek = readStream.peek();
+		expect(next).toBe("a");
+		expect(peek).toBe("b");
+	});
 
-  it("should return true if characters remaining", () => {
-    const hasNext = readStream.hasNext();
-    expect(hasNext).toBe(true);
-  });
+	it("should return true if characters remaining", () => {
+		const hasNext = readStream.hasNext();
+		expect(hasNext).toBe(true);
+	});
 
-  it("should return false if no characters remaining", () => {
-    readStream.next();
-    readStream.next();
-    readStream.next();
-    const hasNext = readStream.hasNext();
-    expect(hasNext).toBe(false);
-  });
+	it("should return false if no characters remaining", () => {
+		readStream.next();
+		readStream.next();
+		readStream.next();
+		const hasNext = readStream.hasNext();
+		expect(hasNext).toBe(false);
+	});
 });
