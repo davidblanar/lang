d7e23e6544425941ec9f3e40b5b9474c98220a23
David Blanar <david.blanar@gmail.com>
2020-05-12 16:11:11 +0200

    more examples, did some todos

--- src/index.js
+++ src/index.js
@@ -10,9 +10,22 @@ if (args.length !== 3) {
 	throw new Error("Usage: node src/script.js <file_path>");
 }
 
-const filePath = args[args.length - 1];
-const input = fs.readFileSync(path.resolve(__dirname, filePath)).toString();
+function getLastArrayElement(arr) {
+  return arr[arr.length - 1];
+}
 
+const filePath = getLastArrayElement(args);
+const filePathArr = filePath.split("/");
+const fileName = getLastArrayElement(filePathArr);
+const fileNameArr = fileName.split(".");
+const ext = getLastArrayElement(fileNameArr);
+if (ext !== "l") {
+  throw new Error(`Unsupported file type ${filePath}`);
+}
+
+const fileManipulationPathPrefix = filePathArr.filter((val, i) => i !== filePathArr.length - 1).join("/");
+
+const input = fs.readFileSync(path.resolve(__dirname, filePath)).toString();
 const readStream = new ReadStream(input);
 const tokenizer = new Tokenizer(readStream);
 const tokens = tokenizer.generateTokens().getTokens();
@@ -37,14 +50,12 @@ env.add("throw_error", function (e) {
 	throw new Error(e);
 });
 env.add("read_file", function (filePath) {
-	// TODO fix "files/" - make it dynamic
 	return fs
-		.readFileSync(path.resolve(__dirname, "files/", filePath))
+		.readFileSync(path.resolve(__dirname, fileManipulationPathPrefix, filePath))
 		.toString();
 });
 env.add("write_file", function (filePath, data) {
-	// TODO fix "files/" - make it dynamic
-	return fs.writeFileSync(path.resolve(__dirname, "files/", filePath), data);
+	return fs.writeFileSync(path.resolve(__dirname, fileManipulationPathPrefix, filePath), data);
 });
 
 evalUnderEnv(ast, env);
